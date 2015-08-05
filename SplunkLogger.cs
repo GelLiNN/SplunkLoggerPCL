1f71b93d379292a68dafbf9bd97fb07336e92eb3
Kellan Nealy <chikn42@gmail.com>
2015-08-04 18:14:57 -0700

    Fixed up some stuff, added check to see if level is "OFF" before logging

--- SplunkLogger.cs
+++ SplunkLogger.cs
@@ -16,9 +16,11 @@ namespace SplunkClient
 		private string uri;
 		private HttpClient client; 
 		private bool sslEnabled;
+        private string sourcetype;
 
 		// Keeps track of severity levels
 		private List<string> levels;
+
 
 		/*
 		* Constructs a SplunkLogger with (most importantly), URI and Http Event Collector token */
@@ -37,6 +39,7 @@ namespace SplunkClient
 			levels.Add ("VERBOSE");
 			levels.Add ("WARNING");
 			this.level = "INFO";
+            this.sourcetype = "Mobile Application";
 
 			sslEnabled = ssl;
 			/*
@@ -49,7 +52,7 @@ namespace SplunkClient
 
 		/*
 		* Tells SplunkLogger whether or not to enable SSL */
-		public void enableSSL ()
+		public void EnableSSL ()
 		{
 			/* Should enable SSL in DIFFERENT WAY if Splunk deployment isn't self signed
 			 * otherwise, this code will automatically validate server cert
@@ -63,7 +66,7 @@ namespace SplunkClient
 
 		/*
 		* Changes the severity level of this SplunkLogger */
-		public void setLevel (string newLevel)
+		public void SetLevel (string newLevel)
 		{
             string levelVal = newLevel.ToUpper();
 			if (levels != null && levels.Contains(levelVal)) {
@@ -71,25 +74,45 @@ namespace SplunkClient
 			}
 		}
 
-		/*
-		* Logs a string message/event to Splunk's Http Event Collector */
-		public void Log (string message)
+        /*
+        * Changes the sourcetype of this SplunkLogger */
+        public void SetSourcetype(string type)
+        {
+            this.sourcetype = type;
+        }
+
+        /*
+		* Logs a string message/event to Splunk's Http Event Collector 
+        * Sequential, less verbose, swallows exceptions */
+        public void Log (string message)
 		{
-			string JSONstr = "{\"event\":{\"message\":\"" + message + "\", \"severity\":\"" + level + "\", \"sourcetype\":\"Mobile Application\"}}";
-			HttpContent content = new StringContent(JSONstr);
-			var resp = (HttpResponseMessage) client.PostAsync(
-				this.uri, content).Result;
+            if (!string.Equals(this.level, "OFF"))
+            {
+                string JSONstr = "{\"event\":{\"message\":\"" + message 
+                    + "\", \"severity\":\"" + level + "\"}, \"sourcetype\":\"" + sourcetype + "\"}";
+
+                HttpContent content = new StringContent(JSONstr);
+                try
+                {
+                    var resp = (HttpResponseMessage)client.PostAsync(
+                        this.uri, content).Result;
+                }
+                catch { }
+            }
 		}
 
 		/*
-		* Logs a string message/event to Splunk's Http Event Collector Async */
+		* Logs a string message/event to Splunk's Http Event Collector, Async */
 		async public Task LogAsync (string message)
 		{
-			string JSONstr = "{\"event\":{\"message\":\"" + message + "\", \"severity\":\"" + level + "\"}}";
-			HttpContent content = new StringContent(JSONstr);
+            if (!string.Equals(this.level, "OFF"))
+            {
+                string JSONstr = "{\"event\":{\"message\":\"" + message + "\", \"severity\":\"" + level + "\"}, \"sourcetype\":\"Mobile Application\"}";
+                HttpContent content = new StringContent(JSONstr);
 
-			var resp = await client.PostAsync (this.uri, content);
-			resp.EnsureSuccessStatusCode ();
+                var resp = await client.PostAsync(this.uri, content);
+                resp.EnsureSuccessStatusCode();
+            }
 		}
 	}
 }
